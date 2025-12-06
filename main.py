ab8b92b1650480d1ad57e7f1c63ce8636d2d391d
Alex Rechsteiner <user.email=769693+arechste@users.noreply.github.com>
2025-12-06 16:21:30 +0100

    add num_words func

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 
-print("greetings boots")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    #print(text)
+    count_words_in_book(text)
+
+def get_book_text(filepath):
+    #contents = ""
+    with open(filepath) as f:
+        return f.read()
+
+def count_words_in_book(text):
+    num_words = len(text.split())
+    print(f"Found {num_words} total words")
+
+main()
